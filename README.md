1c82d01375a046ea262c9a90c9feec7b26945cc3
Jonathan Delgado <hi@jon.soy>
2017-03-07 16:45:26 -0300

    update expect on readme

--- README.md
+++ README.md
@@ -17,4 +17,60 @@ Initialize a Bert file (`.bert.js`).
     $ bert init
 
 
+## Espect .bert.js file
+
+```javascript
+const bert = require('bert')
+
+/* EXAMPLES WITH DOCKER */
+/* ==================== */
+
+bert.stage('node')
+// CMD Equivalent: docker create node
+/* Content .bertlook.json
+{
+  "stages": {
+    "node": "36c4536996ca5615dcf9911f068786dc/a441b15fe9a3cf56661190a0b93b9dec7d04127288cc87250967cf3b52894d11"
+  }
+}
+*/
+
+bert.stage('node', {
+  image: 'node:7.7.0'
+})
+// CMD Equivalent: docker create  node:7.7.0
+/* Content .bertlook.json
+{
+  "stages": {
+    "node": "36c4536996ca5615dcf9911f068786dc/a441b15fe9a3cf56661190a0b93b9dec7d04127288cc87250967cf3b52894d11"
+  }
+}
+*/
+
+bert.stage('node', {
+  tag: '7.7.0'
+})
+// CMD Equivalent: docker create node:7.7.0
+/* Content .bertlook.json
+{
+  "stages": {
+    "node": "36c4536996ca5615dcf9911f068786dc/a441b15fe9a3cf56661190a0b93b9dec7d04127288cc87250967cf3b52894d11"
+  }
+}
+*/
+
+bert.stage('pepe', {
+  image: 'node:7.7.0'
+})
+// CMD Equivalent: docker create node:7.7.0
+/* Content .bertlook.json
+{
+  "stages": {
+    "pepe": "36c4536996ca5615dcf9911f068786dc/a441b15fe9a3cf56661190a0b93b9dec7d04127288cc87250967cf3b52894d11"
+  }
+}
+*/
+
+```
+
 
--- bin/bert.js
+++ bin/bert.js
@@ -1,28 +1,60 @@
 #! /usr/bin/env node
 const path = require('path')
 const {jsonToENV} = require('../lib/jsonToENV')
+const dotEnv = require('dotenv')
 
-const argv = require('minimist')(process.argv.slice(3), {
+const argv = require('minimist')(process.argv.slice(2), {
   boolean: [
-    'help'
+    'help',
+    'skip-env'
   ],
   alias: {
-    help: 'h'
+    help: 'h',
+    env: 'e'
+  },
+  default: {
+    'skip-dotenv': false,
+    'skip-env': false
   }
 })
 
+if (argv['skip-env'] !== false) {
+  process.env = {}
+}
+
+if (argv['skip-dotenv'] !== false) {
+  dotEnv.config({silent: true})
+}
+
+/* Parse Opts env */
+const argvEnv = [].concat(process.env, argv.env.map((e) => dotEnv.parse(e)))
+.reduce((c, n) => {
+  return Object.assign(c, n)
+})
+
+argv.env = argvEnv
+
+// process.env = []
+// console.log( require('dotenv').config( { silent: true } ).parsed )
+
+// console.log(argv)
+
 function setup (argv) {
-  jsonToENV('npm_package' , path.join(__dirname, '../package.json'))
+  // jsonToENV('npm_package' , path.join(__dirname, '../package.json'))
 
   const args = [...argv._]
 
-  const help = () => require('../lib/help').call(null, args, argv)
+  const help = () => require('../lib/help').call(this, args, argv)
 
   if (argv.help) {
     return help()
   }
 
   switch (args.shift()) {
+    case 'i':
+    case 'info':
+      return require('../lib/info').call(this, args, argv)
+
     case 'help':
     default:
       return help()
@@ -30,3 +62,4 @@ function setup (argv) {
 }
 
 setup(argv)
+
