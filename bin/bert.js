94a7708b76945548208e51e0e9056a29d0cf346e
Jonathan Delgado <hi@jon.soy>
2017-03-16 21:59:05 -0300

    Bert Cli: Added harmony support.

--- bin/bert.js
+++ bin/bert.js
@@ -1,92 +1,118 @@
 #! /usr/bin/env node
+
+const spawn = require('child_process').spawn
 const path = require('path')
-const {jsonToENV} = require('../lib/jsonToENV')
-const dotEnv = require('dotenv')
-const dotBert = require('../lib/dotBert')
-const debugLib = require('../lib/debug')
 
-const argv = require('minimist')(process.argv.slice(2), {
-  boolean: [
-    'help',
-    'verbose',
-    'skip-env'
-  ],
-  alias: {
-    help: 'h',
-    env: 'e',
-    verbose: 'V',
-  },
-  default: {
-    env: [],
-    verbose: false,
-    workdir: null,
-    'skip-dotenv': false,
-    'skip-env': false,
-    'bertfile': '.bert.js'
-  }
-})
+const args = ['--harmony-async-await', path.join(__dirname, 'bert_.js'), ...process.argv.slice(2)]
 
-if (argv['skip-env'] !== false) {
-  process.env = {}
-}
+console.log(args)
 
-if (argv['skip-dotenv'] === false) {
-  dotEnv.config({silent: true})
-}
+const proc = spawn(process.execPath, args, { stdio: 'inherit' })
 
-/* Parse Opts ENV */
-const argvEnv = [].concat(
-  process.env,
-  [].concat(argv.env)
-    .map( (e) => dotEnv.parse(e) )
-).reduce((c, n) => {
-  return Object.assign(c, n)
-})
-
-argv.env = argvEnv
-argv.bertConfig = dotBert.setup({silent: true, bertfile: argv.bertfile, workdir: argv.workdir})
-
-argv.debug = debugLib({verbose: argv.verbose})
-
-if (argv.bertConfig.workdir) {
-  argv.debug.verbose('Change workdir to "%s"', argv.bertConfig.workdir)
-  process.chdir(argv.bertConfig.workdir)
-}
-
-
-/**
- * This is a setup function.
- *
- * @param {object}   argv   - The arguments by line command. Rreference of minimist 
- * @param {String[]} argv._
- */
-async function setup (argv) {
-  // clone the argv parameter
-  const args = [...argv._]
-
-  const help = () => require('../lib/help').call(this, args, argv)
-
-  let arg = args.shift()
-
-  switch ( arg ) {
-    case 'i':
-    case 'info':
-      return require('../lib/info').call(this, args, argv)
-
-    case undefined:
-    case 'help':
-      return help()
-
-    case 'run-task':
-    case 'run':
-      return require('../lib/run-task').call(this, args, argv)
-
-    default:
-      return require('../lib/run-task').call(this, [arg, ...args], argv)
-  }
-}
-
-setup(argv)
-  .catch(err => {
-    console.error(err)
+proc.on('exit', function (code, signal) {
+  process.on('exit', function () {
+    if (signal) {
+      process.kill(process.pid, signal)
+    } else {
+      process.exit(code)
+    }
   })
+})
+
+// terminate children.
+process.on('SIGINT', function () {
+  proc.kill('SIGINT')
+  proc.kill('SIGTERM')
+})
+
+// #! /usr/bin/env node
+// const path = require('path')
+// const {jsonToENV} = require('../lib/jsonToENV')
+// const dotEnv = require('dotenv')
+// const dotBert = require('../lib/dotBert')
+// const debugLib = require('../lib/debug')
+
+// const argv = require('minimist')(process.argv.slice(2), {
+//   boolean: [
+//     'help',
+//     'verbose',
+//     'skip-env'
+//   ],
+//   alias: {
+//     help: 'h',
+//     env: 'e',
+//     verbose: 'V',
+//   },
+//   default: {
+//     env: [],
+//     verbose: false,
+//     workdir: null,
+//     'skip-dotenv': false,
+//     'skip-env': false,
+//     'bertfile': '.bert.js'
+//   }
+// })
+
+// if (argv['skip-env'] !== false) {
+//   process.env = {}
+// }
+
+// if (argv['skip-dotenv'] === false) {
+//   dotEnv.config({silent: true})
+// }
+
+// /* Parse Opts ENV */
+// const argvEnv = [].concat(
+//   process.env,
+//   [].concat(argv.env)
+//     .map( (e) => dotEnv.parse(e) )
+// ).reduce((c, n) => {
+//   return Object.assign(c, n)
+// })
+
+// argv.env = argvEnv
+// argv.bertConfig = dotBert.setup({silent: true, bertfile: argv.bertfile, workdir: argv.workdir})
+
+// argv.debug = debugLib({verbose: argv.verbose})
+
+// if (argv.bertConfig.workdir) {
+//   argv.debug.verbose('Change workdir to "%s"', argv.bertConfig.workdir)
+//   process.chdir(argv.bertConfig.workdir)
+// }
+
+// /**
+//  * This is a setup function.
+//  *
+//  * @param {object}   argv   - The arguments by line command. Rreference of minimist
+//  * @param {String[]} argv._
+//  */
+// async function setup (argv) {
+//   // clone the argv parameter
+//   const args = [...argv._]
+
+//   const help = () => require('../lib/help').call(this, args, argv)
+
+//   let arg = args.shift()
+
+//   switch ( arg ) {
+//     case 'i':
+//     case 'info':
+//       return require('../lib/info').call(this, args, argv)
+
+//     case undefined:
+//     case 'help':
+//       return help()
+
+//     case 'run-task':
+//     case 'run':
+//       return require('../lib/run-task').call(this, args, argv)
+
+//     default:
+//       return require('../lib/run-task').call(this, [arg, ...args], argv)
+//   }
+// }
+
+// setup(argv)
+//   .catch(err => {
+//     console.error(err)
+//   })
---
+++ bin/bert_.js
@@ -0,0 +1,9 @@
+async function run () {
+  // await new Promise(r=>setTimeout(r,3000))
+
+  console.log('End run()')
+}
+
+
+
+run()
