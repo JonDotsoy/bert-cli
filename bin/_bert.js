9cce8f6ac1352c72eac47f1cb01b3a61728dcfdb
Jonathan Delgado <hi@jon.soy>
2017-03-19 00:00:56 -0300

    Added suport to workdir on system.

--- bin/_bert.js
+++ bin/_bert.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
-
 const path = require('path')
+const querystring = require('querystring')
+const fs = require('fs')
+
 const every = require('lodash/every')
 const argv = require('minimist')(process.argv.slice(2), {
   default: {
@@ -20,8 +22,11 @@ localBert.on('task_start', function ({task}) {
 
 localBert.on('task_stop', function ({task, duration}) {
   if (countRunClears === 0) {
-    localBert.start('clear containers')
+    process.nextTick(() => {
+        localBert.start('clear containers')
+    })
   }
+
   console.log(logger.stopTask(task, duration))
 })
 
@@ -34,7 +39,7 @@ localBert.task('clear containers', async () => {
   countRunClears += 1
 
   if (every(getTasks().map(task => localBert.tasks[task].done), Boolean)) {
-    await Promise.all( Object.keys(localBert.agents).map(agent => localBert.agents[agent].rmContainer({silent: true})) )
+    await Promise.all(Object.keys(localBert.agents).map(agent => localBert.agents[agent].rmContainer({silent: true})))
   }
 })
 
@@ -45,8 +50,8 @@ async function run () {
   /* Load task bert */
   require(bertfile)
 
-  getTasks().map(e=>localBert.start(e))
+  getTasks().map(e => localBert.start(e))
 }
 
 run()
-.catch(err => console.error(err))
+.catch(err => console.error(err.stack))
